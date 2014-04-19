911eef1bb80116881ad3ed261056455789de7e2b
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2014-04-19 19:43:51 +0200

    (Changes that were already mentioned in previous commit, but not commited.)

--- Martijn_additions/MW_identifycolony_test.m
+++ Martijn_additions/MW_identifycolony_test.m
@@ -15,7 +15,7 @@ output_name = ['SM' img_name]
 %img_location = 'D:\LocalPlayground\Vincent_van_Gogh_(1853-1890)_-_Wheat_Field_with_Crows_(1890).jpg'
 [original,my_colormap] = imread([my_dir img_name]);
 
-figure (1), imshow(original,my_colormap);
+%figure (1), imshow(original,my_colormap);
 
 % Let's modify the img a little bit
 % create working copy
@@ -31,6 +31,20 @@ work_img = 1 - work_img;
 work_img = im2bw(work_img);
 
 
+% Determine average again__________________________________________________
+
+h=figure (1), imshow(normalized_original,my_colormap);
+
+% Select region from which to determine average
+my_rect = round(getrect(h))
+% Rename vars for clearity - note counter-intuitive x/y 
+my_xmin = my_rect(1); my_ymin = my_rect(2); 
+my_xmax = my_rect(1)+my_rect(3); my_ymax = my_rect(2)+my_rect(4);
+
+% Get area
+my_avg_area = normalized_original(my_ymin:my_ymax,my_xmin:my_xmax);
+% Get average
+my_mean = mean(my_avg_area(:));
 
 % _________________________________________________________________________
 
@@ -38,7 +52,7 @@ work_img = im2bw(work_img);
 
 % Create a disk-shaped structuring element with a radius of 5 pixels.
 
-se = strel('disk',5);
+se = strel('disk',35);
 
 % Remove snowflakes having a radius less than 5 pixels by opening it with the disk-shaped structuring element.
 
@@ -57,17 +71,20 @@ work_img = bwareaopen(work_img, 500);
 
 % perform operations
 work_img = bwareaopen(work_img, 500);
-work_img = imopen(work_img,se);
+work_img = imclose(work_img,se);
+work_img = bwareaopen(work_img, 3500);
+work_img = imdilate(work_img, se);
 
 % determine filter
-filter = work_img+1.0;
+filter = work_img;
 
 % show filter
 img_after = work_img;
 figure (3), imshow(filter);
 
 % apply to orginal
-work_img = normalized_original ./ filter; % (work_img now contains ones and twos)
+work_img = normalized_original .* filter; % (work_img now contains ones and twos)
+work_img(find(work_img==0)) = my_mean;
 % renormalize
 work_img = work_img ./ max(work_img(:)); 
 

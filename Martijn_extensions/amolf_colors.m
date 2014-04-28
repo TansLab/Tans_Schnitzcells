a2c27e4d04442298f5dcc0e537a44e3739eebb48
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2014-04-28 15:18:38 +0200

    General convenient files.

---
+++ Martijn_extensions/amolf_colors.m
@@ -0,0 +1,5 @@
+% colors
+colorAmolfGreen = [110/255 255/255 0/255];
+colorAmolfBlue = [0/255 145/255 255/255];
+colorAmolfDarkGreen = [0/255 181/255 60/255];
+colorAmolfYellow = [218/255 242/255 0/255];

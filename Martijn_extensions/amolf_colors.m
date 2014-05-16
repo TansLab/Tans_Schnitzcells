eba2c63ea8921ceed1c2ecd591b92691db15881d
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2014-05-16 14:28:17 +0200

    Moved files to other repo.

--- Martijn_extensions/amolf_colors.m
+++
@@ -1,5 +0,0 @@
-% colors
-colorAmolfGreen = [110/255 255/255 0/255];
-colorAmolfBlue = [0/255 145/255 255/255];
-colorAmolfDarkGreen = [0/255 181/255 60/255];
-colorAmolfYellow = [218/255 242/255 0/255];

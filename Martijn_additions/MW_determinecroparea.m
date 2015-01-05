0d49d94117a0d2c3558b302ad1f29169a91f4bca
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-01-05 15:15:12 +0100

    (determineCropArea) Little scripts that aids you in determining which area to crop out that is relevant.

---
+++ Martijn_additions/MW_determinecroparea.m
@@ -0,0 +1,41 @@
+function MW_determinecroparea(p, range)
+% 'little helper' function determinecroparea(p, range)
+% ===
+% Tries to determine last image in range, and simply calls getrect for that
+% to then output leftTop and rightBottom positions for cropping.
+
+    % Determine and load last phase image
+    DlastPhase = dir([p.imageDir, [p.movieName,'-p-*' num2str(range(end)) '.tif'] ]);
+    imToLoad = DlastPhase(round(numel(DlastPhase)/2));
+    myImg = imread([p.imageDir imToLoad.name]);
+    myImgSize = size(myImg);
+
+    % Show figure and getrect
+    figure, imshow(myImg,[])
+    myCrop = round(getrect());
+
+    % Get leftTop and rightBottom of cropping region
+    leftTop = myCrop(1:2)
+    rightBottom = myCrop(1:2)+ myCrop(3:4)
+
+    % Correct for even/uneven requirements
+    if mod(leftTop(1),2) == 0 % if even
+        leftTop(1) =  leftTop(1) -1 ; % make uneven
+    end
+    if mod(leftTop(2),2) == 0 % if even
+        leftTop(2) =  leftTop(2) -1 ; % make uneven
+    end
+    if mod(rightBottom(1),2) == 1 % if uneven
+        rightBottom(1) =  rightBottom(1) +1 ; % make even
+        if rightBottom(1) > myImgSize(2), rightBottom(1) = rightBottom(1) - 2; end % make sure not to expand beyond img size
+    end
+    if mod(rightBottom(2),2) == 1 % if uneven
+        rightBottom(2) =  rightBottom(2) +1 ; % make even
+        if rightBottom(2) > myImgSize(1), rightBottom(2) = rightBottom(2) - 2; end % make sure not to expand beyond img size
+    end        
+    % output to user
+    disp('This function only outputs via disp(), cut and paste below values into Excel:')
+    disp(['leftTop=' mat2str(leftTop) '; rightBottom=' mat2str(rightBottom)])
+
+
+end

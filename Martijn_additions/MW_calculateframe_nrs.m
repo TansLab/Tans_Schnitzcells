f9c91b43fc681e4981e8944ea5855779d5f88052
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-04-20 13:27:52 +0200

    (N+1bug) Function that provides backwards compatibility with old "frames" parameter.

---
+++ Martijn_additions/MW_calculateframe_nrs.m
@@ -0,0 +1,27 @@
+function schnitzcells = MW_calculateframe_nrs(schnitzcells)
+% 
+% The Schnitzcells structure has an index for the frames with the images of
+% the bacterial oclony. This index used to be called "frames", but this
+% index was shifted +1. This has been removed, and to avoid conflicts, the
+% index now has a new name, which is "frame_nrs". 
+% Some old schnitzcells files still use the "frames" as index, and this
+% function - very straightforwardly - converts the old index to the new,
+% and adds it to the schnitzcells structure.
+% 
+% INPUT
+% Schnitzcells struct
+%
+% OUTPUT
+% Schnitzcells struct
+
+
+for i = 1:numel(schnitzcells)
+    
+    % backwards compatibility
+    if isfield(schnitzcells,'frames')
+        schnitzcells(i).frame_nrs = schnitzcells(i).frames-1; % N+1 bug
+    end
+    
+end
+
+end

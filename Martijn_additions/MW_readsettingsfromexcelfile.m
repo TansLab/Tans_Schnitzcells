12727dbd450589d870c303b96d7c2f7e37ee4404
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-01-08 20:15:58 +0100

    (GUI) update

--- Martijn_additions/MW_readsettingsfromexcelfile.m
+++ Martijn_additions/MW_readsettingsfromexcelfile.m
@@ -1,23 +1,37 @@
-function [settings, alldata] = MW_readsettingsfromexcelfile(configfilepath)
+function [settings, alldata] = MW_readsettingsfromexcelfile(settings)
+    % Input argument required:
+    % struct settings that has a field called configfilepath, which holds
+    % the location of the configfile.
+
 
     % configfilepath = CONFIGFILEPATH
 
     %% read in excel file (note that we start reading at line 14)
-    [ndata, text, alldata] = xlsread(configfilepath,'Configuration','A14:B100');
+    [ndata, text, alldata] = xlsread(settings.configfilepath,'Configuration','A14:B100');
 
     %% Create the variables that are listed in the configuration file
     % ===
     % alldata{i, 1} contains the desired names of the parameters, alldata{i, 2}
     % contains the values of the parameter.
+    processedFieldNames = {};
     for i = 1:size(alldata,1)
         if ~(isempty(alldata{i, 1}) || isempty(alldata{i, 2}) || any(isnan(alldata{i, 2})) )
+            
+            % put value in a var
+            parameterValue = alldata{i, 2}; 
+            
             % Tell user
             disp(['Processing ' alldata{i, 1}]);
-
+            disp(['Value = ' parameterValue ]);
+            
+            % Double check whether it's not double (leads to hairy situations)
+            if any(strcmp(processedFieldNames,alldata{i, 1}))
+                error('There are duplicate field names in Excel file, please correct!')
+            end
+            
             %% create a parameter with the name contained by alldata{i, 1} 
             % (= left column of excel sheet), with value parameterValue.                
-            % ===
-            parameterValue = alldata{i, 2}; % put value in a var
+            % ===            
             if isnumeric(parameterValue) 
                 % if it is a numeric value, convert it to string w. numeric
                 % value
@@ -37,8 +51,11 @@ function [settings, alldata] = MW_readsettingsfromexcelfile(configfilepath)
             end
             
             %% Use the eval command to produce a parameter with the current value
-            command = ['settings.' alldata{i, 1} '=' parameterValue];
+            command = ['settings.' alldata{i, 1} '=' parameterValue ';']
             eval (command); 
+            
+            % Administration
+            processedFieldNames{end+1} = alldata{i, 1};
         end
     end    
 

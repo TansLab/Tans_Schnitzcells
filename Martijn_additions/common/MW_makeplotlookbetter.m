cb08a7b8ba463fd8b42dc9d2bf5763840de6a1f4
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-02-22 18:41:58 +0100

    (minorcosmetic) Comment to recommend use of linspecer to generate plot line colors.

--- Martijn_additions/common/MW_makeplotlookbetter.m
+++ Martijn_additions/common/MW_makeplotlookbetter.m
@@ -4,6 +4,9 @@ function MW_makeplotlookbetter(FontSize)
 %
 % Currently only sets all font sizes to <FontSize>. 
 % Maybe in future add some more conveniences.
+%
+% One can use function "linspecer" to generate colors that are
+% distinguishable by human eye.
 
 %Set all fontsizes
 set(findall(gcf,'type','text'),'FontSize',FontSize,'fontWeight','normal')

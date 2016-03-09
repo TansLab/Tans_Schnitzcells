ec1cf253e399db671658ac8318ddeede4521be60
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-03-09 13:16:33 +0100

    (Documentation) Updated documentation inside masterscript.

--- Schnitzcells_masterscript.m
+++ Schnitzcells_masterscript.m
@@ -1,9 +1,52 @@
 
 %% INSTRUCTIONS
 
-% Set the filepath of the configuration file (by setting CONFIGFILEPATH 
-% below), and then run this script by pressing "run and advance" for each 
-% section.
+% This file will allow you to perform all of the analyses that we generally
+% perform to analyse growing microcolonies of cells. 
+% 
+% For each dataset, for each microcolony, an Excel file should be present
+% that holds the parameter settings for the analysis. An example excel file
+% should be present in the root directory. I suggest you copy this Excel
+% file, rename it such that you can relate it to your dataset (current
+% format Schnitzcells_Config_<experiment-date>_<microcolony-pos#>), and
+% edit it to your needs.
+%
+% If you have edited your Excel file appropriately, continue with the
+% analysis below by executing the script section by section. The parameter
+% "runsections", which is set automatically, decides which sections are
+% executed.
+% In fact, if you execute the script without any settings, a GUI will open
+% that allows you to automatically run the script section by section. The
+% matlab file associated with this GUI is:
+% >> MW_GUI_schnitzcells 
+% The main output of the analysis (aside from plots) is the data file which
+% contains the "schnitzcells" struct. Once generated, it will be located 
+% in pos1\data\pos1-Schnitz.mat. It contains the lineage structure with all
+% the separate individual cells (i.e. birth-to-division, aka one 
+% "schnitz").
+% 
+% 
+% A more extensive description of what the matlab scripts do can be found
+% in the comments on each section.
+%
+% MW, 2016/03
+% 
+% ===
+%
+% This software was originally developed in the Elowitz group, see:
+% Young, J. W., Locke, J. C. W., Altinok, A., Rosenfeld, N., Bacarian, T., 
+% Swain, P. S., … Elowitz, M. B. (2012). Measuring single-cell gene 
+% expression dynamics in bacteria using fluorescence time-lapse microscopy. 
+% Nature Protocols, 7(1), 80–8. doi:10.1038/nprot.2011.432
+%
+% It has been extensively edited and extended in the tans biophysics lab.
+% It is currently stored at (private repository):
+% https://bitbucket.org/microscopeguerrillas/schnitzcells_tans
+% To request access contact the Tans Biophysics lab,
+% http://www.amolf.nl/research/biophysics/
+% http://tansgroup.amolf.nl/
+% Martijn "MW" Wehrens, wehrens@amolf.nl; PI: Sander Tans, tans@amolf.nl.
+
 
 %% TODOs
 
@@ -18,49 +61,72 @@
 %% Run this section to start the GUI interface
 % (Press ctrl+enter when cursor is here.)
 
-
 if ~exist('runsections','var') || strcmp(runsections, 'none')
 
     runsections = 'none';
-    Schnitzcells_GUI
+    MW_GUI_schnitzcells
 
 end
 
-%% parameters to set when running manually are in capitals
+%% Parameters to set before starting.
+% Set settings.analysisType and press CTRL+enter.
+% The settings.analysisType parameter allows you to do a full analysis
+% (standard), or preliminary analysis. The preliminary analysis does not
+% track lineages, and allows you to get an impression of the whole colony
+% behavior. (The preliminary analysis will create a schnitzcells file, but
+% this file will not allow for a appropriate full analysis of your data.)
+%
+% settings.analysisType     should be 'preliminary' or 'full' to
+%                           automatically run the appropriate sections for
+%                           a preliminary or full analysis.
+
 
 % declaration of necessary global paramteters (for running from GUI)
 global p settings
 
-% in case you DO NOT want to run from GUI, the following code is
-% automatically executed when runsections is not set
+% In case you DO NOT want to run from GUI, the following code is
+% automatically executed when runsections is not set by the GUI
 if ~exist('runsections', 'var')
             
     % Parameters to set
     % ==
     
     % Full or prelinary analysis
-    settings.analysisType = 'preliminary'; % preliminary or full
-    %settings.analysisType = 'full';
-    
-    LOADDATASETATEND = 1;
+    if ~isfield(settings,'analysisType')
+        % settings.analysisType = 'preliminary'; % preliminary or full
+        settings.analysisType = 'full';
+    end   
 
-    % Parameters not to be altered
+    % Set the runsections parameter appropriately
     if strcmp(settings.analysisType, 'preliminary')
         runsections = 'allpreliminary'; % all is default
     elseif strcmp(settings.analysisType, 'full')
         runsections = 'allfull';        
     end
     
+    % (re)load data at end of analysis from disk if set to 1.
+    LOADDATASETATEND = 1;
+    
+    % Signal to script whether has been run from GUI
     ranFromGUI = 0;
 else
     ranFromGUI = 1;
 end
 
-% More parameters you SHOULD NOT change
-settings.EXCELREADSTART = 14; % line where list of parameters starts in Excel file.
-settings.MYTEXTEDITOR = 'notepad.exe';
+% Parameters you should NOT change
+% ===
+% line where list of parameters starts in Excel file.
+settings.EXCELREADSTART = 14; 
+% text editor that matlab will attempt to open to show you files.
+settings.MYTEXTEDITOR = 'notepad.exe'; 
 
-%% It is very important to supply the path to the correct dataset here!
+
+%% Choose the Excel file with settings for your dataset.
+% Press CTRL+enter.
+% Run this section and a dialogue box will allow you to choose an Excel
+% file. This section simply saves the path of the file you have chosen to
+% settings.configfilepath.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull' 'loadfile'}))
     
     %select the configfile to run
@@ -75,6 +141,9 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull' 'loadfile'}))
 end
 
 %% Read configuration settings from excel file.
+% Press CTRL+enter.
+% The function MW_readsettingsfromexcelfile reads in the Excel file, and
+% puts all settings in a struct called "settings".
 
 if any(strcmp(runsections,{'allpreliminary', 'allfull', 'loadfile','reloadfile'}))
     % One can execute this section again to reload settings 
@@ -91,7 +160,9 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull', 'loadfile','reloadfile'}
 end
 
 %% Double check whether to continue
+% Press CTRL+enter.
 % This step is mainly to prevent accidentally running whole script 
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull'}))
         
     analysisTypes = {'PRELIMINARY','FULL'}
@@ -104,6 +175,10 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull'}))
 end
 
 %% Now make a vector with parameter values that schnitzcells scripts can handle. (and some misc. other admin)
+% Press CTRL+enter.
+% Schnitzcells takes the struct "p" as input. This is basically a less
+% extended version of the settings struct. "p" is set here.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','createp'}))
     
     disp('Now creating ''p'' struct from settings struct.');
@@ -131,21 +206,32 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','createp'}))
     
 end
 %% Some more hard coded parameters
+% Press CTRL+enter.
+% Set an ouput directory (mainly for the preliminary analysis).
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','createp'}))
 
-    % Output directory for figures etc.
+    % Output directory for figures etc.    
     settings.MYOUTPUTDIR = [p.dateDir 'outputSummary\'];
     
 end
 
 %% Crop images
+% Press CTRL+enter.
+% This section allows you to crop the images. If you don't want to crop the
+% images, you need to manually copy images from the appropriate data 
+% directory to a subdirecotry called ./images/.
 
 % Crop images 
 % =========================================================================
 
+% Ask user whether cropping is desired.
+settings.performCropping = strcmp(questdlg('Do you want to perform cropping of images on your dataset?','Cropping','Yes','No','No'),'Yes');
+
 % Crop images
 % Puts cropped images in new directory, which gets the suffix 
 if any(strcmp(runsections,{'allpreliminary','cropimages'}))
+if settings.performCropping
     
     if strcmp(settings.analysisType, 'preliminary')
 
@@ -193,11 +279,16 @@ if any(strcmp(runsections,{'allpreliminary','cropimages'}))
         
     end
     
+end  
 end
 
 
 %% Update p accordingly.
+% Press CTRL+enter.
+% If you have cropped the images the p struct needs to be updated with the
+% new location of the images. 
 
+if settings.performCropping
 if any(strcmp(runsections,{'allpreliminary', 'allfull','cropimages','loadpforcropped'}))
 
     % =========================================================================
@@ -213,11 +304,17 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','cropimages','loadpforcro
     disp('p (parameters) loaded');
 
 end
+end
+
 %% Segmenation
+% Press CTRL+enter.
+% The function PN_segmoviephase_3colors will segment all images in the
+% framerange provided by settings.currentFrameRange.
 % =========================================================================
 % ADVANCED PARAMETER SETTINGS that can be useful:
 % p.useFullImage=1; % forces to use full image for segmentation
-% p.overwrite=1; % overwrite existing files
+% p.overwrite=1; % overwrite existing files (to redo segmentation)
+% =========================================================================
 
 if any(strcmp(runsections,{'allpreliminary', 'allfull','segmentation'}))
 
@@ -239,6 +336,11 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','segmentation'}))
 end
 
 %% To manually redo 1 frame, execute this code manually or via GUI
+% Press CTRL+enter.
+% This section is generally not executed; only when a frame's segmentation
+% has failed, you can execute this code that allows you to redo that frame
+% with different parameters.
+
 if any(strcmp(runsections,{'redosegforframe'}))   
     if ~ranFromGUI
         % EDIT PARAMETERS HERE IF MANUALLY REDOING
@@ -279,6 +381,17 @@ end
 
 
 %% Start the manual checking by user
+% Press CTRL+enter.
+% The segmentation performed by the Matlab script often contains errors.
+% There is an extensive script to interact with the segmentation files from
+% each frame, in order to correct errors in the segmentation. The is done
+% by the function PN_manualcheckseg, which will provide a list of commands
+% that allow you to navigate and edit through the segmentation files.
+% The segmentation files can be found in the ./segmentation directory, and
+% contain the raw segmentation (LNsub) plus - after running
+% PN_manualcheckseg - the corrected segmentation that is used for the
+% analysis in the matrix Lc.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','manualchecksegfull','manualchecksegfull'}))
 
     % choose option based type analysis
@@ -293,6 +406,10 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','manualchecksegfull','man
 
 end
 %% Perform quick analysis of segmentation
+% Press CTRL+enter.
+% If you're doing a preliminary analysis, you can run DJK_analyzeSeg to get
+% an impression of the growth behavior of the colony.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','quickanalysis'}))
     
     DJK_analyzeSeg(p,'manualRange',settings.currentFrameRange,'onscreen',1,'DJK_saveDir',settings.MYOUTPUTDIR);
@@ -303,9 +420,28 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','quickanalysis'}))
 end
 
 %% Perform tracking, check tracking, make problem movie and correct
+% Press CTRL+enter. To reiterate: press CTRL+enter.
 % Iterate this section until no problems are listed any more in:
 % \analysis\tracking\manualRangeX_Y\posZcrop-tracking.txt
-% iterate these commands (also partially redundant w. above) to correct segmentation
+% This analysis will track cells from frame to frame (using the default
+% function DJK_tracker_djk, see next section for alternatives), thus
+% identifying lineages. However, often there are mistakes in this procedure. 
+% These can often be corrected by correcting the segmentation. Therefor,
+% this section also tries to identify suspicious events in the lineage, and
+% suspicous cells are listed in aformentioned text file. Consequtively, the
+% segmentation correction can be run again. When p.problemCells is set it
+% will highlight the suspicious cells listed there. One can press "s" to
+% give frame or schnitzcells cell numbers. When all frames are manually
+% corrected, one can run this section again to see whether all problems
+% have disappeared. (Note that suspicious cells not always turn out to be
+% wrongly tracked.)
+% If you have identified schnitzes (indivual cells), or lineages, that show
+% issues (e.g. artefacts that cannot be removed), the cells can be marked
+% as "bad schnitzes" in the Excel file. Lineages, or lineages containing
+% these cells, will later be excluded from the analysis.
+% Here, for the first time the schnitzcells file is generated, which
+% contains all the output of the analyses. This file can be fuond in
+% posX\data\posX-Schnitz.mat
 
 if any(strcmp(runsections,{'allpreliminary','trackpreliminary'})) % fast
     DJK_trackcomplete(p,'trackRange',settings.currentFrameRange,'trackMethod','singleCell');
@@ -352,6 +488,15 @@ elseif any(strcmp(runsections,{'allfull','trackandmanualcorrections'}))% full
 end
 
 %% (Optional) Some alternative trackers
+% Press CTRL+enter. Re-iterate previous section when done.
+% The code contains two alternative trackers, the defualt tracker is
+% DJK_tracker_djk, but two alternatives are MW_tracker (fast, works by
+% overlap, high failure rate if cells move a load), and
+% NW_tracker_centroid_vs_area (slow, but often offers good tracking where 
+% DJK_tracker_djk has failed).
+% This section will only be run if customtrackersoncustomrange is set.
+% Set settings.specialtracker to run a desired alternative tracker (either 
+% MW or NW). If this field is not set, default tracker is run.
 
 if any(strcmp(runsections,{'customtrackersoncustomrange'}))
    
@@ -366,7 +511,12 @@ if any(strcmp(runsections,{'customtrackersoncustomrange'}))
     
 end
 
+
 %% (Optional) Check again after alternative tracker
+% <TODO>
+% If in previous section alternative tracker was called, the tracking also
+% needs to be checked for suspcious cells. Do this with the earlier
+% section. This section is under construction.
 
 if any(strcmp(runsections,{'checkaftercustom'}))
     
@@ -398,7 +548,9 @@ if any(strcmp(runsections,{'checkaftercustom'}))
 end
 
 %% Once above section has been iterated to satisfaction make sure to remove problemCells field from p
+% Press CTRL+enter after having reiterated earlier steps to satisfaction.
 % (Because movies later also work with this.)
+
 if any(strcmp(runsections,{'allfull','makemovie'}))
     
     % remove problemCells parameter from p if it exists (because otherwise
@@ -410,6 +562,12 @@ if any(strcmp(runsections,{'allfull','makemovie'}))
 end
 
 %% Make movie that allows you to double check the segmentation & tracking
+% Press CTRL+enter.
+% This sections creates a movie of the growing microcolony, with extra
+% (color) information about lineages. Check it to see whether all mistakes
+% in identifying lineages have been removed. The movie is exported to the
+% analysis directory, subdirectory ./movies, but this folder is opened
+% automatically when script is done.
 
 if any(strcmp(runsections,{'allfull','makemovie'}))
 
@@ -437,11 +595,19 @@ if any(strcmp(runsections,{'allfull','makemovie'}))
 end
 
 %% Create backup of segtrack
+% Press CTRL+enter.
+% Creates a backup of the tracking.
+
 if any(strcmp(runsections,{'allfull','createbackup'}))
     NW_Backup_SegTrack(p);
 end
 
 %% Fluor part I: Start of correcting fluor
+% Press CTRL+enter.
+% Some initializing functions are executed here, but the most important
+% function is DJK_correctFluorImage_anycolor, which corrects fluor images
+% with shading, background, etc.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}))
     MAXSHIFT = 10;
 
@@ -490,6 +656,10 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}
 end
 
 %% Lengths: Now calculate lengths and add them to schnitzcells struct
+% Press CTRL+enter.
+% Functions executed here calculate the length and growth rate, of all of 
+% the cells in the microcolony. DJK_addToSchnitzes_length calculates
+% length, DJK_addToSchnitzes_mu calculates growth rate.
 % Some of the fluorescence analysis also needs the fields generated here
 
 if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}))
@@ -510,6 +680,10 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}
 
 end
 %% Fluor part II: Continue with fluor again now
+% Press CTRL+enter.
+% Add values of fluor concentration, fluor production to the schnitzcells
+% struct.
+
 if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}))
 
     % Again loop over colors
@@ -613,6 +787,10 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull','correctionsandanalysis'}
 end
 
 %% For preliminary analysis, create some data output
+% Press CTRL+enter.
+% Setting up the schnitzcells struct is done now. 
+% If you were doing a preliminary analysis, this
+% section will provide some analysis. 
 %  ===
 
 if any(strcmp(runsections,{'allpreliminary','analysispreliminary'})) 
@@ -683,6 +861,8 @@ end
 
 
 %% DONE / LOADING DATA
+% Press CTRL+enter.
+% Load data if desired.
 
 if any(strcmp(runsections,{'allpreliminary', 'allfull', 'makeoutputfull'}))
 
@@ -710,6 +890,13 @@ if any(strcmp(runsections,{'allpreliminary', 'allfull', 'makeoutputfull'}))
 
 end
 %% More analyses for full analysis
+% Press CTRL+enter.
+% This section provides a load of analysis of the schnitzcells structure.
+% It calculates noise values and cross-correlations of important
+% parameters. It is convenient to save this analysis to a database 
+% with all your experiments. The script savingFluorDynamicsData provides
+% you with this opportunity (see section "Database" below).
+
 PLOTSCATTER=0; % activate to plot all scatter plots
 
 if any(strcmp(runsections,{'allfull', 'makeoutputfull', 'rerunfullanalysis'})) % full
@@ -838,7 +1025,7 @@ if any(strcmp(runsections,{'allfull', 'makeoutputfull','rerunfullanalysis'})) % 
 end
 
 
-%% If desired, save to appropriate database
+%% Database. If desired, save to appropriate database
 
 % Additional options for preliminary analysis
 % MW_summaryplotspreliminaryanalysis

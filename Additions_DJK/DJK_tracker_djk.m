5ea8679bf039d9f0940e8127ef57b76a1d1e7cf7
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-04-25 19:24:58 +0200

    (fluorshift) Changed what happens when the shift goes out of the image.

--- Additions_DJK/DJK_tracker_djk.m
+++ Additions_DJK/DJK_tracker_djk.m
@@ -522,44 +522,47 @@ startx  = minfx+offset_x;
 endx    = maxfx+offset_x;
 % Now it could fall outside the image, in that case, just crop it
 % This is a bit of a boilerplate solution.. - MW
+cropErrorFlag = 0;
 if starty<1
-    warning('starty<1; MIGHT LEAD TO ERROR W. FLUOR LATER.. Centered image falls outside boundaries, cropping image..');
+    warning('starty<1; Will not perform shift!');
     howmuchoutofplace=1-starty
     starty=1
-    minfy = minfy+howmuchoutofplace
+    %minfy = minfy+howmuchoutofplace
+    % Resetting values
+    cropErrorFlag = 1;
 end
 if startx<1
-    warning('startx<1; MIGHT LEAD TO ERROR W. FLUOR LATER.. Centered image falls outside boundaries, cropping image..');
+    warning('startx<1; Will not perform shift!');
     howmuchoutofplace=1-startx
     startx=1
-    minfx = minfx+howmuchoutofplace
+    %minfx = minfx+howmuchoutofplace
+    cropErrorFlag = 1;
 end
 if endy>phaseFullSize(1)
-    warning('endy>phaseFullSize(1); MIGHT LEAD TO ERROR W. FLUOR LATER.. Centered image falls outside boundaries, cropping image..');
+    warning('endy>phaseFullSize(1); Will not perform shift!');
     howmuchoutofplace=phaseFullSize(1)-endy
     endy=phaseFullSize(1)
-    maxfy = maxfy+howmuchoutofplace
+    %maxfy = maxfy+howmuchoutofplace
+    cropErrorFlag = 1;
 end
 if endx>phaseFullSize(2)
-    warning('endx>phaseFullSize(1); MIGHT LEAD TO ERROR W. FLUOR LATER.. Centered image falls outside boundaries, cropping image..');
+    warning('endx>phaseFullSize(1); Will not perform shift!');
     howmuchoutofplace=phaseFullSize(2)-endx
     endx=phaseFullSize(2)
-    maxfx = maxfx+howmuchoutofplace
+    %maxfx = maxfx+howmuchoutofplace
+    cropErrorFlag = 1;
 end
-% Remark MW 2015/08
-% Thought this would fix things, but dataset where this bugfix was used
-% for, later showed errors in fluor handling. TODO!
-% remark NW (2012-08)/Edited MW 2015/08: 
-% If warningmessage here: possible that colony is so
-% large that after centering the outer coordinates of cells exceed the
-% image range (e.g. become negative). Another solution than current one: 
-% pretend that the
-% original image is the centered image (e.g. if the image was not cropped
-% during segmentation):
-%Lc_fullsize_centered=Lc_fullsize;
+% Remark MW 2016/04
+% When new center is such that to-select-area falls outside original image
+% when performing shift, don't perform the shift..
+% Previous solution was to crop, but that resulted into issues.
 
 % Do the centering
-Lc_fullsize_centered( starty:endy, startx:endx ) = Lc_fullsize( minfy:maxfy, minfx:maxfx );
+if ~cropErrorFlag
+    Lc_fullsize_centered( starty:endy, startx:endx ) = Lc_fullsize( minfy:maxfy, minfx:maxfx );
+else
+    Lc_fullsize_centered=Lc_fullsize;
+end;
 
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%

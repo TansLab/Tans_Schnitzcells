8886e2bfa7a36f0ae4fdd95e2e265fa3eecf405d
Nick_de_Lange <nickdelange@ziggo.nl>
2016-04-28 14:45:16 +0200

    Code for producing dataset of rotated images

---
+++ Additions_NDL/NDL_Image_rotation.m
@@ -0,0 +1,23 @@
+A=imread('F:\Datasets\test_rotate_72x5degrees\pos2\pos2-p-1-000.tif');
+for i=1:71
+    B=imrotate(A,5*i,'bilinear','crop');
+    if i<10
+        imwrite(B,['F:\Datasets\test_rotate_72x5degrees\pos2\pos2-p-1-00' num2str(i) '.tif']);
+    elseif i<100
+        imwrite(B,['F:\Datasets\test_rotate_72x5degrees\pos2\pos2-p-1-0' num2str(i) '.tif']);
+    else
+        imwrite(B,['F:\Datasets\test_rotate_72x5degrees\pos2\pos2-p-1-' num2str(i) '.tif']);
+    end
+end
+%%
+C=imread('F:\Datasets\test_rotate_72x5degrees\pos1\pos1-p-1-000.tif');
+for i=1:71
+    D=imrotate(C,5*i,'bilinear','crop');
+    if i<10
+        imwrite(D,['F:\Datasets\test_rotate_72x5degrees\pos1\pos1-p-1-00' num2str(i) '.tif']);
+    elseif i<100
+        imwrite(D,['F:\Datasets\test_rotate_72x5degrees\pos1\pos1-p-1-0' num2str(i) '.tif']);
+    else
+        imwrite(D,['F:\Datasets\test_rotate_72x5degrees\pos1\pos1-p-1-' num2str(i) '.tif']);
+    end
+end

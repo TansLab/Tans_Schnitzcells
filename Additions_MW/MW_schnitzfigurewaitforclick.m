c8b56689c076311047684c609bf4bef4a3378792
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-04-29 18:04:15 +0200

    (manualcheckseg) cosmetic update

---
+++ Additions_MW/MW_schnitzfigurewaitforclick.m
@@ -0,0 +1,25 @@
+
+function MW_schnitzfigurewaitforclick(src,callbackdata)
+
+%{
+['global pos Limage ourfig res pp phfig;pos=max(1,round((1/res)*get(gca,''CurrentPoint'')));',...
+                'if (pos(1,2)>0 & pos(1,2)<size(Limage,1) & pos(1,1)>0 & pos(1,1)<size(Limage,2));',...
+                'curr_val=num2str(double(Limage(pos(1,2),pos(1,1))));else;curr_val=''-1'';end;',...
+                'set(ourfig,''name'',[''Pos: '',num2str(pos(1,2)),'' , '',num2str(pos(1,1)),',...
+                '''  Val: '',curr_val]);']
+%}
+
+global pos Limage ourfig res pp phfig;
+
+pos=max(1,round((1/res)*get(gca,'CurrentPoint')));
+
+if (pos(1,2)>0 & pos(1,2)<size(Limage,1) & pos(1,1)>0 & pos(1,1)<size(Limage,2))
+    curr_val=num2str(double(Limage(pos(1,2),pos(1,1))));
+else
+    curr_val='-1';
+end;
+
+set(ourfig,'name',['Pos: ',num2str(pos(1,2)),' , ',num2str(pos(1,1)),'  Val: ',curr_val]);
+            
+            
+end

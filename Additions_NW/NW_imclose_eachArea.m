17642d727a20bddfe4d454b4445f5c8a68efc653
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2017-06-01 12:29:12 +0200

    (performance) made imclose function on frame 2x more efficient

--- Additions_NW/NW_imclose_eachArea.m
+++ Additions_NW/NW_imclose_eachArea.m
@@ -13,22 +13,68 @@ function outputIm=NW_imclose_eachArea(inputIm,struct_el)
 %           E.g. strel('disk',5)
 %
 
+MARGIN=10;
+
 outputIm=inputIm;
 
-% find indices of areas (typically 0,1,2,...)
+%% find indices of areas (typically 0,1,2,...)
 AreaNrs=unique(inputIm)';  % need row vector
 idx=find(AreaNrs~=0); % background=0
 AreaNrs=AreaNrs(idx);
 clear idx
+
+
 % find integers (via modulo because 'unique' has double as output???)
-idxnotint=find(mod(AreaNrs,1))~=0;
-if ~isempty(idxnotint)
+% idxnotint=find(mod(AreaNrs,1))~=0;
+%if ~isempty(idxnotint)
+if any(mod(AreaNrs,1))
     error('Some areas have non-integer values!')
 end
 
-%loop over all areas and fill fuzzy edges (imclose)
-for runidx=AreaNrs
+%% loop over all areas and fill fuzzy edges (imclose)
+%tic
+sizeInputIm=size(inputIm); % MW
+for runidx=AreaNrs   
+   
+    %%
+    % The 3 commented lines below are shorter in script, but take 2X as
+    % long as code below
+    %{
     SubAreaonlyIm=(inputIm==runidx);
     SubAreaonlyIm=imclose(SubAreaonlyIm,struct_el);
     outputIm(SubAreaonlyIm==1)=runidx;
+    %}
+    
+     
+    
+    %%    
+    % make a little image containig the cell of interest
+    [i,j]=ind2sub(sizeInputIm,find(inputIm==runidx));
+    toSelecti = [max(1,min(i)-MARGIN):min(max(i)+MARGIN,sizeInputIm(1))];
+    toSelectj = [max(1,min(j)-MARGIN):min(max(j)+MARGIN,sizeInputIm(2))];    
+    SubAreaonlyIm = inputIm(toSelecti, toSelectj);
+    SubAreaonlyIm = (SubAreaonlyIm==runidx);
+    
+    % close it
+    SubAreaonlyIm=imclose(SubAreaonlyIm,struct_el);
+    
+    % update output image accordingly
+    for idxi = 1:numel(toSelecti)
+        for idxj = 1:numel(toSelectj)
+            
+            i=toSelecti(idxi);
+            j=toSelectj(idxj);
+            
+            if SubAreaonlyIm(idxi,idxj)
+                outputIm(i, j) = runidx;
+            end
+            
+        end
+    end
+    
+
 end
+%toc
+
+% figure(); imshow(outputIm,[]);
+
